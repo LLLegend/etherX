3a546a0c03230cf9f72cac57a1fb79ed6a446fce
LLLegend <chuanqiliu99@outlook.com>
2024-02-18 17:38:50 +0800

    test connection

--- parser/main.go
+++ parser/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/ethereum/go-ethereum/rpc"
 	"log"
 	"math/big"
 	"time"
@@ -21,6 +22,8 @@ func main() {
 	blockNumber := big.NewInt(3000000)
 	start := time.Now()
 	header, err := client.HeaderByNumber(context.Background(), blockNumber)
+	blockHash := header.Hash()
+	numTx, err := client.TransactionCount(context.Background(), blockHash)
 	fmt.Println("Block Number: ", header.Number)
 	fmt.Println("Block Hash: ", header.Hash())
 	fmt.Println("Block Coinbase: ", header.Coinbase)
@@ -28,6 +31,16 @@ func main() {
 	fmt.Println("Block Time: ", header.Time)
 	fmt.Println("Tx Hash: ", header.TxHash)
 	fmt.Println("size: ", header.Size())
+	fmt.Println("numTx: ", numTx)
+
+	bHash := rpc.BlockNumberOrHash{BlockHash: &blockHash}
+	receipts, err := client.BlockReceipts(context.Background(), bHash)
+	fmt.Println("Block Receipts: ", receipts)
+
+	tx, err := client.TransactionInBlock(context.Background(), blockHash, 1)
+	txReceipt, err := client.TransactionReceipt(context.Background(), tx.Hash())
+	fmt.Println("tx Receipts: ", txReceipt)
+
 	fmt.Println("Using ", time.Since(start))
 
 }
