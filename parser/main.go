a1a819ba2e2de13091faccaaca532fe2c71d9c1d
LLLegend <chuanqiliu99@outlook.com>
2024-02-18 14:13:31 +0800

    test connection

---
+++ parser/main.go
@@ -0,0 +1,28 @@
+package parser
+
+import (
+	"context"
+	"fmt"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"log"
+	"math/big"
+)
+
+func main() {
+	ipcPath := "/home/node01/Documents/eth-data/geth.ipc"
+
+	client, err := ethclient.Dial(ipcPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected")
+
+	blockNumber := big.NewInt(3000000)
+	header, err := client.HeaderByNumber(context.Background(), blockNumber)
+	fmt.Println("Block Number: ", header.Number)
+	fmt.Println("Block Hash: ", header.Hash())
+	fmt.Println("Block Coinbase: ", header.Coinbase)
+	fmt.Println("Block gasUsed: ", header.GasUsed)
+
+}
