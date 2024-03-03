5bdc35111c745d3eb0aaed34f219f38e983f53e6
LLLegend <chuanqiliu99@outlook.com>
2024-03-03 23:05:27 +0800

    test

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 mkdir -p bin
 
 go build -o bin/test src/main.go src/configs.go src/getLevelDB.go
-go build -o bin/test2 src/collectDataFromGeth.go src/configs.go src/dbconnections.go src/sql.go src/types.go requests.go
+go build -o bin/test2 src/collectDataFromGeth.go src/configs.go src/dbconnections.go src/sql.go src/types.go src/requests.go
 
 chmod 777 bin/test
 chmod 777 bin/test2
