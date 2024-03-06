6fcf38783467cdcb18e37d33479a575826e3a015
LLLegend <chuanqiliu99@outlook.com>
2024-03-06 18:19:55 +0800

    test

--- src/main.go
+++ src/main.go
@@ -3,94 +3,96 @@ package main
 import (
 	"bytes"
 	"fmt"
-	"github.com/cockroachdb/pebble"
-	"github.com/cockroachdb/pebble/bloom"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/rlp"
-	"runtime"
 )
 
 func main() {
-	leveldbPath = "/home/node01/Documents/eth-data/geth/chaindata/ancient/chain"
+	pebblePath := "/home/node01/Documents/eth-data/geth/chaindata/"
+	pebbleAncientPath := "/home/node01/Documents/eth-data/geth/chaindata/ancient/chain"
 
 	fmt.Println("------------------ Get data from pebble-------------------")
 
-	cache := 2048
-	handles := 2048
-
-	// The max memtable size is limited by the uint32 offsets stored in
-	// internal/arenaskl.node, DeferredBatchOp, and flushableBatchEntry.
+	//cache := 2048
+	//handles := 2048
 	//
-	// - MaxUint32 on 64-bit platforms;
-	// - MaxInt on 32-bit platforms.
+	//// The max memtable size is limited by the uint32 offsets stored in
+	//// internal/arenaskl.node, DeferredBatchOp, and flushableBatchEntry.
+	////
+	//// - MaxUint32 on 64-bit platforms;
+	//// - MaxInt on 32-bit platforms.
+	////
+	//// It is used when slices are limited to Uint32 on 64-bit platforms (the
+	//// length limit for slices is naturally MaxInt on 32-bit platforms).
+	////
+	//// Taken from https://github.com/cockroachdb/pebble/blob/master/internal/constants/constants.go
+	//maxMemTableSize := (1<<31)<<(^uint(0)>>63) - 1
 	//
-	// It is used when slices are limited to Uint32 on 64-bit platforms (the
-	// length limit for slices is naturally MaxInt on 32-bit platforms).
+	//// Two memory tables is configured which is identical to leveldb,
+	//// including a frozen memory table and another live one.
+	//memTableLimit := 2
+	//memTableSize := cache * 1024 * 1024 / 2 / memTableLimit
 	//
-	// Taken from https://github.com/cockroachdb/pebble/blob/master/internal/constants/constants.go
-	maxMemTableSize := (1<<31)<<(^uint(0)>>63) - 1
-
-	// Two memory tables is configured which is identical to leveldb,
-	// including a frozen memory table and another live one.
-	memTableLimit := 2
-	memTableSize := cache * 1024 * 1024 / 2 / memTableLimit
-
-	// The memory table size is currently capped at maxMemTableSize-1 due to a
-	// known bug in the pebble where maxMemTableSize is not recognized as a
-	// valid size.
+	//// The memory table size is currently capped at maxMemTableSize-1 due to a
+	//// known bug in the pebble where maxMemTableSize is not recognized as a
+	//// valid size.
+	////
+	//// TODO use the maxMemTableSize as the maximum table size once the issue
+	//// in pebble is fixed.
+	//if memTableSize >= maxMemTableSize {
+	//	memTableSize = maxMemTableSize - 1
+	//}
 	//
-	// TODO use the maxMemTableSize as the maximum table size once the issue
-	// in pebble is fixed.
-	if memTableSize >= maxMemTableSize {
-		memTableSize = maxMemTableSize - 1
-	}
+	//opt := &pebble.Options{
+	//	// Pebble has a single combined cache area and the write
+	//	// buffers are taken from this too. Assign all available
+	//	// memory allowance for cache.
+	//	Cache:        pebble.NewCache(int64(cache * 1024 * 1024)),
+	//	MaxOpenFiles: handles,
+	//
+	//	// The size of memory table(as well as the write buffer).
+	//	// Note, there may have more than two memory tables in the system.
+	//	MemTableSize: uint64(memTableSize),
+	//
+	//	// MemTableStopWritesThreshold places a hard limit on the size
+	//	// of the existent MemTables(including the frozen one).
+	//	// Note, this must be the number of tables not the size of all memtables
+	//	// according to https://github.com/cockroachdb/pebble/blob/master/options.go#L738-L742
+	//	// and to https://github.com/cockroachdb/pebble/blob/master/db.go#L1892-L1903.
+	//	MemTableStopWritesThreshold: memTableLimit,
+	//
+	//	// The default compaction concurrency(1 thread),
+	//	// Here use all available CPUs for faster compaction.
+	//	MaxConcurrentCompactions: func() int { return runtime.NumCPU() },
+	//
+	//	// Per-level options. Options for at least one level must be specified. The
+	//	// options for the last level are used for all subsequent levels.
+	//	Levels: []pebble.LevelOptions{
+	//		{TargetFileSize: 2 * 1024 * 1024, FilterPolicy: bloom.FilterPolicy(10)},
+	//		{TargetFileSize: 2 * 1024 * 1024, FilterPolicy: bloom.FilterPolicy(10)},
+	//		{TargetFileSize: 2 * 1024 * 1024, FilterPolicy: bloom.FilterPolicy(10)},
+	//		{TargetFileSize: 2 * 1024 * 1024, FilterPolicy: bloom.FilterPolicy(10)},
+	//		{TargetFileSize: 2 * 1024 * 1024, FilterPolicy: bloom.FilterPolicy(10)},
+	//		{TargetFileSize: 2 * 1024 * 1024, FilterPolicy: bloom.FilterPolicy(10)},
+	//		{TargetFileSize: 2 * 1024 * 1024, FilterPolicy: bloom.FilterPolicy(10)},
+	//	},
+	//	ReadOnly: false,
+	//}
+	//opt.EnsureDefaults()
+	//// Disable seek compaction explicitly. Check https://github.com/ethereum/go-ethereum/pull/20130
+	//// for more details.
+	//opt.Experimental.ReadSamplingMultiplier = -1
 
-	opt := &pebble.Options{
-		// Pebble has a single combined cache area and the write
-		// buffers are taken from this too. Assign all available
-		// memory allowance for cache.
-		Cache:        pebble.NewCache(int64(cache * 1024 * 1024)),
-		MaxOpenFiles: handles,
+	//db, err := pebble.Open(leveldbPath, opt)
+	//if err != nil {
+	//	fmt.Println(err)
+	//	panic(err)
+	//}
+	//fmt.Println("Pebble open successfully")
 
-		// The size of memory table(as well as the write buffer).
-		// Note, there may have more than two memory tables in the system.
-		MemTableSize: uint64(memTableSize),
-
-		// MemTableStopWritesThreshold places a hard limit on the size
-		// of the existent MemTables(including the frozen one).
-		// Note, this must be the number of tables not the size of all memtables
-		// according to https://github.com/cockroachdb/pebble/blob/master/options.go#L738-L742
-		// and to https://github.com/cockroachdb/pebble/blob/master/db.go#L1892-L1903.
-		MemTableStopWritesThreshold: memTableLimit,
-
-		// The default compaction concurrency(1 thread),
-		// Here use all available CPUs for faster compaction.
-		MaxConcurrentCompactions: func() int { return runtime.NumCPU() },
-
-		// Per-level options. Options for at least one level must be specified. The
-		// options for the last level are used for all subsequent levels.
-		Levels: []pebble.LevelOptions{
-			{TargetFileSize: 2 * 1024 * 1024, FilterPolicy: bloom.FilterPolicy(10)},
-			{TargetFileSize: 2 * 1024 * 1024, FilterPolicy: bloom.FilterPolicy(10)},
-			{TargetFileSize: 2 * 1024 * 1024, FilterPolicy: bloom.FilterPolicy(10)},
-			{TargetFileSize: 2 * 1024 * 1024, FilterPolicy: bloom.FilterPolicy(10)},
-			{TargetFileSize: 2 * 1024 * 1024, FilterPolicy: bloom.FilterPolicy(10)},
-			{TargetFileSize: 2 * 1024 * 1024, FilterPolicy: bloom.FilterPolicy(10)},
-			{TargetFileSize: 2 * 1024 * 1024, FilterPolicy: bloom.FilterPolicy(10)},
-		},
-		ReadOnly: false,
-	}
-	opt.EnsureDefaults()
-	// Disable seek compaction explicitly. Check https://github.com/ethereum/go-ethereum/pull/20130
-	// for more details.
-	opt.Experimental.ReadSamplingMultiplier = -1
-
-	db, err := pebble.Open(leveldbPath, opt)
-	if err != nil {
-		fmt.Println(err)
-		panic(err)
-	}
-	fmt.Println("Pebble open successfully")
+	config := OpenOptions{Type: dbPebble, Directory: pebblePath, AncientsDirectory: pebbleAncientPath, Namespace: "eth/db/chaindata/", Cache: 2048,
+		Handles: 2048, ReadOnly: false, Ephemeral: false}
+	db, _ := openKeyValueDatabase(config)
 
 	var num uint64
 	num = 3000000
@@ -100,24 +102,22 @@ func main() {
 	blkHashKey := getBlockHeaderHashKey(num)
 	fmt.Println("HeaderHashKey: ", blkHashKey)
 
-	dat, closer, err := db.Get(blkHashKey)
+	dat, err := db.Get(blkHashKey)
 	if err != nil {
 		fmt.Println(err)
 		panic(err)
 	}
 	ret := make([]byte, len(dat))
 	copy(ret, dat)
-	closer.Close()
 
 	fmt.Println("2121221")
 	// Get Block Hash from Key
-	blkHash, closer, err := db.Get(blkHashKey)
+	blkHash, err := db.Get(blkHashKey)
 	if err != nil {
 		fmt.Println("error")
 		panic(err)
 	}
 	fmt.Println("44444")
-	err = closer.Close()
 	if err != nil {
 		panic(err)
 	}
@@ -127,9 +127,8 @@ func main() {
 
 	headerKey := getBlockHeaderKey(num, blkHash)
 	//
-	blkHeaderData, closer, _ := db.Get(headerKey) // headerKey是新的key
-	//
-	err = closer.Close()
+	blkHeaderData, _ := db.Get(headerKey) // headerKey是新的key
+
 	if err != nil {
 		return
 	}
