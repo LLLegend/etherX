88721bac892653dced752351c033fcd8650cbd97
LLLegend <chuanqiliu99@outlook.com>
2024-03-06 18:21:44 +0800

    test

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 mkdir -p bin
 
-go build -o bin/test src/main.go src/configs.go src/getLevelDB.go
-go build -o bin/test2 src/collectDataFromGeth.go src/configs.go src/dbconnections.go src/sql.go src/types.go src/requests.go src/utils.go src/pebble.go
+go build -o bin/test src/main.go src/configs.go src/getLevelDB.go src/pebble_eth.go
+go build -o bin/test2 src/collectDataFromGeth.go src/configs.go src/dbconnections.go src/sql.go src/types.go src/requests.go src/utils.go
 
 chmod 777 bin/test
 chmod 777 bin/test2
